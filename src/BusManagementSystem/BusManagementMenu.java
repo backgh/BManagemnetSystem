aa59121d7f255be86d93ac57deb6bb6ff166c92a
LG PC <LG PC@BackghPC>
2022-03-14 13:49:05 +0900

    added BusManagementMenu

---
+++ src/BusManagementSystem/BusManagementMenu.java
@@ -0,0 +1,9 @@
+package BusManagementSystem;
+
+public class BusManagementMenu {
+	public static void main(String[] args) {
+		
+		System.out.println(
+				"1.Add Bus 2.Delete Bus 3.Edit Bus 4.View Bus 5.Exit");
+	}
+}
